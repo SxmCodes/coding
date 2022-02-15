85e50d37d86a1151928e28ad4d1a1a942a391587
Saksham Sharma <sakshamsharma1029@gmail.com>
2022-02-15 16:13:57 +0530

    strings in java, introduction.

--- CWH/java/strings/strings.java
+++ CWH/java/strings/strings.java
@@ -1,10 +1,14 @@
 public class strings{
     public static void main(String[] args) {
-        /** NOTES
-         * Strings are not primitive datatypes in java but we can use it as primitive.
+        /**                             NOTES
+         * Strings are not primitive datatypes in java but we can use it as primitive 🙂
+         * This is the example in which I had made an object 😃
+            String name = new String("Hello world.");
+            // We can also write string name = "lght", this will print lght.
+            System.out.println(name);
+         *  
+         * Strings cannot be changed in the memory.
          * 
          */
-        String name = new String("Hello world.");
-        System.out.println(name);
     }
 }
