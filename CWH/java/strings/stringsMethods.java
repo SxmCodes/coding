851a6ab6d0c4ca8d6825c2c1217aef26fe7cbd83
Saksham Sharma <sakshamsharma1029@gmail.com>
2022-02-26 18:47:43 +0530

    New changes.

---
+++ CWH/java/strings/stringsMethods.java
@@ -0,0 +1,10 @@
+public class stringsMethods {
+    public static void main(String[] args) {
+        /*          NOTES
+        1. length() -> Returns the length of the string.
+         */
+
+        // length()
+          
+    }
+}
